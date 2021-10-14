a27e4aa29b61e06e6bff619a028dfcf9f157e96b
@RubenGarciaOrdaz <rgo1999@hotmail.com>
2021-10-14 16:43:01 -0500

    Adding report

--- src/main/java/sistemasdistribuidos/tecnm/mx/scanner/nmapPirata.java
+++ src/main/java/sistemasdistribuidos/tecnm/mx/scanner/nmapPirata.java
@@ -7,9 +7,7 @@ package sistemasdistribuidos.tecnm.mx.scanner;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.ArrayList;
